10b6cd027f0fc10feddfb31c0fb06c3a88a94a0e
Andy Liu <andyliuandy@gmail.com>
2015-10-14 11:48:54 -0400

    Mirrors placed, movement done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rbody;
+	Vector3 inputVector;
+
+	float moveSpeed = 5f;
+	float turnSpeed = 2f;
+	float gas;
+	float turn;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		gas = Input.GetAxis("Vertical");
+		turn = Input.GetAxis("Horizontal") * turnSpeed * gas;
+		inputVector = new Vector3(0f, 0f, gas * moveSpeed);
+		
+	}
+	
+	void FixedUpdate() {
+		float yVel = rbody.velocity.y;
+		if ( gas != 0 ) {
+			//Debug.Log ( "input: " + Input.GetAxis("Horizontal"));
+			//Debug.Log ( "turnSpeed: " + turnSpeed);
+			//Debug.Log ( "gas: " + gas);
+			//Debug.Log ( "turn: " + turn);
+			rbody.transform.eulerAngles += new Vector3 (0f, turn, 0f);
+			
+			rbody.velocity = transform.TransformDirection(inputVector);
+		}
+		rbody.velocity += new Vector3(0f, yVel, 0f);
+	}
+}
