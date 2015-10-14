9be33369eda4521cca52236e0005dd83c1712d51
Andy Liu <andyliuandy@gmail.com>
2015-10-14 12:50:47 -0400

    Mirrors corrected (flipped)

---
+++ Assets/Scripts/InvertCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvertCamera : MonoBehaviour {
+
+	Camera cam;
+	
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+
+	void OnPreCull() {
+		cam.ResetWorldToCameraMatrix();
+		cam.ResetProjectionMatrix();
+		cam.projectionMatrix = cam.projectionMatrix * Matrix4x4.Scale (new Vector3(-1f, 1f, 1f));
+	}
+
+	// called before render, set to true so that the image is shown
+	void OnPreRender() {
+		GL.SetRevertBackfacing(true);
+	}
+
+	// called after render, set to false so that it does not affect other cameras
+	void OnPostRender() {
+		GL.SetRevertBackfacing(false);
+	}
+
+}
